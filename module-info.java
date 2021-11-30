62ce1d29315e702fff0871aca06ca3cefe2d737e
Jorge Luís María <94793401+jlmaria@users.noreply.github.com>
2021-11-30 17:34:05 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,11 @@
+module Saludador_FX {
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+
+    opens hellofx;
+}
